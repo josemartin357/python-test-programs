84e1dc9912603dde86497a8bffe0b446cc3e3cd0
Martin <martin.bedoya2021@gmail.com>
2022-05-05 01:40:42 -0700

    Added pseudo code and notes to program for future reference

--- world-cup-simulator/tournament.py
+++ world-cup-simulator/tournament.py
@@ -1,4 +1,4 @@
-# Simulate a sports tournament
+# Function outputs the chances of a team winning a tournament. It does so by simulating a tournament 1000 times, and by repeatedly simulating rounds until we are left with one team.
 
 import csv
 import sys
@@ -8,55 +8,61 @@ import random
 N = 1000
 
 def main():
-    # Ensure correct usage
+    # command line argument needs to have 2 inputs. For example: python3 tournament.py 2018m.csv
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
+    # empty list of teams
     teams = []
-    # teams need to read from csv all teams and sort teams inside, so we use a dictionary
-    # opening file using argument vector, in reading mode. For example, to run: python3 tournament.py 2018m.csv
+    # opening file using argument vector (eg: 2018m.csv), in reading mode. 
     with open(sys.argv[1], "r") as file:
-        # turning file into dictionary
+        # variable holds dictionary reader of csv file
         reader = csv.DictReader(file)
-        # taking a look at content in dictionary thru every row
+        # taking a look at content in dictionary in every row
         for row in reader:
-            # defining variable for row team
+            # variable holds values in row 'team'(defined in csv)
             team_name = row['team']
-            # new variable converts teams' rating into an int, since all values from csv are strings by standard
+            # variable converts teams' rating into an int, since all values from csv are strings by standard
             team_rating = int(row['rating'])
-            # appending to teams list
+            # pushing names and ratings to teams list previously empty and forming team and rating keys
             teams.append({'team':team_name, 'rating': team_rating})
         # print(simulate_tournament(teams))
 
-
+    # empty dictionary that will eventually hold winning teams as key and a value (times team has won a tournament)
     counts = {}
-    # counts is a dictionary that stores names of teams as keys and values how many tournaments team has won
-    # looping to simulate N tournaments and keeping track of win counts
+    # looping to simulate N tournaments 
     for i in range(N):
-        # getting name of winning team from simulate_tournament function
+        # getting name of winning team from value returned in simulate_tournament function
         winner_team = simulate_tournament(teams)
+        # if the winner team is already in counts dictionary ...
         if winner_team in counts:
-            # every time a team wins, increase counts by 1
+            # ... then every time a team wins, increase counts of that team by 1
             counts[winner_team] += 1
         else:
-            # this is the first team has won, so counts equal 1
+            # this is the first team has won, so counts for that time equal 1
             counts[winner_team] = 1
 
     # Print each team's chances of winning, according to simulation
+    # sorted sorts teams by counts; key=lambda treats strings as ints to achieve sort, and reverse=true allows to sort in descending order
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
+        #printing with format string each team AND a formula to obtain percentage with 1 decimal
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
-
+# function to simulate game takes two arguments
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
+    # we grab the ratings by going into the team1 and team2 rating key in dictionary teams (reference function simulate_round)
     rating1 = team1["rating"]
     rating2 = team2["rating"]
+    # run probability mathematical equation
     probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
+    # random.random() returns a random floating number between 0 and 1
     return random.random() < probability
 
-
+# function simulates rounds and has teams dictionary being passed on
 def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
+    # start empty list of winners
     winners = []
     # Simulate games for all pairs of teams
     for i in range(0, len(teams), 2):
